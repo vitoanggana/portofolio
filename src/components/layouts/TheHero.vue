78409da28b8157c912b1c340dd4d4a0cd3df3cae
vitoanggana <vitoangganaprawira@gmail.com>
2023-01-09 22:04:10 +0700

    apologize

--- src/components/layouts/TheHero.vue
+++ src/components/layouts/TheHero.vue
@@ -19,6 +19,16 @@
     <p class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose">
       For further information, you can download my <LinkBtn href="/public/Vito Anggana Profile.pdf" download="CV Vito Anggana.pdf">CV</LinkBtn>.
     </p>
+    <p class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose">
+      Oh yea, if you Yui/Aya or whoever is the cutest girl in surabaya/tasik.
+      ごめんね、前にいったことについて本当に申し訳ございません。
+
+      I really dumb, and I said something that I know I would regret later, but yeah now here I am begging for second chance. I told Azka about this, and,, he, was kinda sad, and now I realize how dumb I am for giving up on you.
+      But I wont begging my knees just to get back to you. If you really still have interest in me, and not giving up. I know you would give me another chance.
+
+      しかし、それはすべてあなた次第です。明日の朝まで、待ちます。
+      じゃ。。本当にありがとうございました。
+    </p>
 
   </div>
 </template>
