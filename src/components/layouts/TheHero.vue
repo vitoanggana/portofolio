e1deedc7a29334786809fd29a878084d55b6e04b
vitoanggana <vitoangganaprawira@gmail.com>
2023-01-13 19:12:35 +0700

    ooo

--- src/components/layouts/TheHero.vue
+++ src/components/layouts/TheHero.vue
@@ -19,28 +19,7 @@
     <p class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose">
       For further information, you can download my <LinkBtn href="/public/Vito Anggana Profile.pdf" download="CV Vito Anggana.pdf">CV</LinkBtn>.
     </p>
-    <h1
-      class="text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text">
-      <span class="text-link_text_hover">.</span>
-    </h1>
-    <h1
-      class="text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text">
-      <span class="text-link_text_hover">.</span>
-    </h1>
-    <h1
-      class="text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text">
-      <span class="text-link_text_hover">.</span>
-    </h1>
-    <p class="text-link_text_hover text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose">
-      Oh yea, if you <span class="text-indigo-600">Aya</span> or whoever is the cutest girl in surabaya/tasik.<br>
-      ごめんね、前にいったことについて本当に申し訳ございません。<br>
-
-      I really dumb, and I said something that I know I would regret later, but yeah now here I am begging for second chance. I told Azka about this, and,, he, was kinda sad, and now I realize how dumb I am for giving up on you.
-      But, I wont begging my knees just to get back to you. If you really still have interest in me, and not giving up on me. I know you would give me another chance.<br>
-
-      しかし、それはすべてあなた次第です。明日の朝まで、待ちます。
-      じゃ。。本当にありがとうございました。
-    </p>
+    
 
   </div>
 </template>
